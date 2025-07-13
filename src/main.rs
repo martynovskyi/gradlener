64b1e52bf0b09010392a437861a88fd693b1d125
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-07-13 23:37:43 +0300

    feat: read build file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 
+use std::path::Path;
+use std::fs;
 mod cli_struct;
 use clap::Parser;
 use cli_struct::Args;
@@ -6,4 +8,16 @@ use cli_struct::Args;
 fn main() {
     let args = Args::parse();
     println!("Args: {:?}", args);
+
+    let build_script = read_build_file(&args.file); 
+}
+
+fn read_build_file(file: &String) -> String {
+    let path = Path::new(&file);
+    if path.exists() {  
+        let contents = fs::read_to_string(path).unwrap();
+        println!("{}", contents);
+        return contents;
+    }
+    return String::new();
 }

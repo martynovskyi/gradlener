113093e6f52d4bc08c74779d86eab15f93daef5a
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-07-17 23:26:29 +0300

    feat: read block name

--- src/groovy_dsl/script_index.rs
+++ src/groovy_dsl/script_index.rs
@@ -2,9 +2,37 @@
 pub struct ScriptIndex {
 
 
+
 }
 
 pub fn parse(script: &str) -> ScriptIndex {
+    let mut cursor = 0usize;
+   
+    let mut lexeme_s: usize = 0;
+    let mut lexeme_e: usize = 0;
+    let mut lexeme_read = false;
+    let chars: Vec<char> = script.chars().collect();
+    for c in script.chars() {
+        if c.is_whitespace() {
+             if lexeme_read {
+                lexeme_e = cursor;
+                println!("{} - {:?}", cursor, &script[lexeme_s..lexeme_e]);
+            }
+            lexeme_s = 0;
+            lexeme_e = 0;
+            lexeme_read = false;
+            cursor += 1;
+            continue;
+        }
+
+        if !lexeme_read {
+            lexeme_s = cursor;
+            lexeme_read = true;
+        } else {
+            lexeme_e = cursor;
+        }
+        cursor += 1;
+    }
 
     ScriptIndex { }
 } 

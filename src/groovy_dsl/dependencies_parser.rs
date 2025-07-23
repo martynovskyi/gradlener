4e422fbe20cb8fe77c1229dda70544b3a11236cf
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-07-24 00:36:18 +0300

    feat: add parsers files

---
+++ src/groovy_dsl/dependencies_parser.rs
@@ -0,0 +1,7 @@
+use crate::build_script::dependencies::*;
+
+pub fn parse(script: &str) -> Dependencies {
+
+
+    Dependencies { entries: Vec::new()}
+}
---
+++ src/groovy_dsl/plugin_parser.rs
@@ -0,0 +1,7 @@
+use crate::build_script::plugins::*;
+
+pub fn parse(script: &str) -> Plugins {
+
+
+    Plugins { entries: Vec::new()}
+}

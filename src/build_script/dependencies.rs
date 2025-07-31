5fe236291f86fae7e34acdf41bdc33ee1c7fa131
Vitalii Martynovskyi <vitalii@motokyi.com>
2025-08-01 00:46:52 +0300

    test: setup parser test for short dependencies

--- src/build_script/dependencies.rs
+++ src/build_script/dependencies.rs
@@ -1,14 +1,24 @@
 
+
 #[derive(Clone, Debug)]
 pub struct Dependencies {
     pub entries: Vec<Dependency>,
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub enum NotationType {
+    Short, 
+    MapStyle,
+    Advanced,
+    Unknown
+}
 
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub struct Dependency {
     pub configuration_name: String,
-    pub group: String,
-    pub artifact: String,
-    pub version: String,
+    pub notation_type: NotationType,
+    pub dependency_notation: String,
+    pub group: Option<String>,
+    pub artifact: Option<String>,
+    pub version: Option<String>,
 }
--- src/groovy_dsl/dependencies_parser.rs
+++ src/groovy_dsl/dependencies_parser.rs
@@ -1,6 +1,16 @@
 use crate::build_script::dependencies::*;
 
+
 pub fn parse(script: &str) -> Dependencies {
+
+    if script.find("dependencies").is_some_and(|ind| ind != 0){
+        panic!("Passed script is not a dependencies container");
+    }
+    println!("Input: {}", script);
+
+    let mut entries: Vec<Dependency> = Vec::new();
+
+    let mut not_parsed: &str = script;
 
 
     Dependencies { entries: Vec::new()}
